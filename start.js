8f71aca94effd82c00e65f07585f162cc3664ec9
usamagee028 <144736191+usamagee028@users.noreply.github.com>
2026-04-06 02:38:57 +0500

    Update start.js

--- start.js
+++ start.js
@@ -2,11 +2,12 @@ const { spawn } = require("child_process");
 
 const env = { ...process.env };
 
-console.log(`Starting audio extractor on port ${env.APP_PORT || 3001}`);
-const audioApp = spawn("node", ["/app/index.js"], {
-  stdio: "inherit",
-  env,
-});
+// --- AUDIO EXTRACTOR DISABLED ---
+// console.log(`Starting audio extractor on port ${env.APP_PORT || 3001}`);
+// const audioApp = spawn("node", ["/app/index.js"], {
+//   stdio: "inherit",
+//   env,
+// });
 
 console.log(`Starting n8n on port ${env.PORT || 5678}`);
 const n8n = spawn("n8n", ["start"], {
@@ -14,10 +15,11 @@ const n8n = spawn("n8n", ["start"], {
   env,
 });
 
-audioApp.on("exit", (code) => {
-  console.error(`Audio extractor exited with code ${code}`);
-  process.exit(code || 1);
-});
+// --- AUDIO EXTRACTOR WATCHDOG DISABLED ---
+// audioApp.on("exit", (code) => {
+//   console.error(`Audio extractor exited with code ${code}`);
+//   process.exit(code || 1);
+// });
 
 n8n.on("exit", (code) => {
   console.error(`n8n exited with code ${code}`);
